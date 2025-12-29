8629fdbfd5c0564420726e12ee3b86d9ad208b6b
mayanksrivastava922 <catvideos999yt@gmail.com>
2025-12-29 14:02:36 +0530

    imagekt, food item craeted , middlware , storage service and food routes implemented

---
+++ backend/src/controllers/food.controller.js
@@ -0,0 +1,27 @@
+const foodModel = require('../models/food.model')
+const storageService = require('../services/storage.service')
+const { v4: uuid } = require("uuid")
+
+async function createFood(req, res) {
+
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid())
+
+    // console.log(fileUploadResult)
+
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        foodPartner: req.foodPartner._id
+    })
+
+    res.status(201).json({
+        messgae: "Food Created Successfully",
+        food: foodItem
+    })
+
+}
+
+module.exports = {
+    createFood
+}
